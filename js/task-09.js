6b5b350a24bf47380e353592b0b47bbfcf9df598
RamonaMonica <ramonamonica08@icloud.com>
2023-10-23 23:33:01 +0300

    final commit

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+
+
+const widget = document.querySelector(".widget");
+const changeColorButton = widget.querySelector(".change-color");
+const colorSpan = widget.querySelector(".color");
+
+changeColorButton.addEventListener("click", function() {
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
+
+
+
+

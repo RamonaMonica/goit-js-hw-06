cb2b835f8e498470eb7e9fb1b3be97ae20788ed2
RamonaMonica <ramonamonica08@icloud.com>
2023-10-24 12:49:10 +0300

    modify task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,13 +1,16 @@
 
+let counterValue = 0;
 
-let counterValue = 0; 
-const decrementButton = document.querySelector('[data-action="decrement"]');
-const incrementButton = document.querySelector('[data-action="increment"]');
-incrementButton.addEventListener('click', function() {
-    counterValue += 1;
-    valueElement.textContent = counterValue;
-  });
-  decrementButton.addEventListener('click', function() {
-    counterValue -= 1;
-    valueElement.textContent = counterValue;
-  });
+const DECREMENT_BUTTON = document.querySelector('[data-action="decrement"]'); 
+const INCREMENT_BUTTON = document.querySelector('[data-action="increment"]'); 
+const COUNTER_SPAN = document.getElementById("value"); 
+
+DECREMENT_BUTTON.addEventListener("click", () => { 
+  counterValue--; 
+  COUNTER_SPAN.textContent = counterValue; 
+});
+
+INCREMENT_BUTTON.addEventListener("click", () => { 
+  counterValue++; 
+  COUNTER_SPAN.textContent = counterValue;
+});
